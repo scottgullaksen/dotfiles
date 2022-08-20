d9a4c32c2b684f55e41be654ff249f5131affa86
Scott Gullaksen <scott.gullaksen@drift.oslo.kommune.no>
2022-08-20 15:34:07 +0200

    First commit. Added dotfiles from workmachine.

---
+++ .config/fish/config.fish
@@ -0,0 +1,35 @@
+# disable fish login prompt
+set -U fish_greeting
+
+# aliases
+
+abbr -a ls lsd
+abbr -a l lsd -l
+abbr -a la lsd -la
+abbr -a lt lsd --tree
+
+abbr -a mci mvn clean install
+abbr -a mcp mvn clean package
+abbr -a msr mvn spring-boot:run
+
+abbr -a connect60 ssh -L 22224:sftpfilserveritas.oslofelles.oslo.kommune.no:22 internitas1systest -N
+
+abbr -a k kubectl
+abbr -a kdeploy k8s_helm_deploy.py
+abbr -a kauth k8s-auth-client.sh
+abbr -a ksc k8s-set-context.sh
+abbr -a apt-manualinstalls "comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort -u)"
+abbr -a vpn nmcli --ask con up id # cool, because its an alias, but it can be expanded to the full command!
+
+abbr -a copy xclip -sel clip
+abbr -a cpass lpass show -cp
+abbr -a tmps /home/scottg/scripts/temps
+
+fish_vi_key_bindings  # set vi-mode
+
+# Environment variables (global + exported)
+set -gx GPG_TTY $(tty)
+set -gx EDITOR nvim
+
+# Init the starship prompt
+starship init fish | source
